12e4e65c8195dcc5f47a150b60430c5502b57bec
israeldoamaral <80478844+israeldoamaral@users.noreply.github.com>
2022-03-11 10:53:24 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,9 +5,15 @@ resource "tls_private_key" "key" {
   algorithm = "RSA"
 }
 
-resource "local_file" "private_key" {
-  filename          = "${var.namespace}-key.pem"
-  sensitive_content = tls_private_key.key.private_key_pem
+# resource "local_file" "private_key" {
+#   filename          = "${var.namespace}-key.pem"
+#   sensitive_content = tls_private_key.key.private_key_pem
+#   file_permission   = "0400"
+# }
+
+resource "local_sensitive_file" "private_key" {
+  content  = tls_private_key.key.private_key_pem
+  filename = "${var.namespace}-key.pem"
   file_permission   = "0400"
 }
 
